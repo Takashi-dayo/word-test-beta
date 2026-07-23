75b74a76aaf1b6f510ff4ed6a5c8c6b929aba3bd
Takashi-dayo <oikawa.kyojin@gmail.com>
2026-07-23 14:34:54 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "word-study-app-shell-v7-8";
+const CACHE_NAME = "word-study-app-shell-v7-9";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./spell-dictionary.js?v=7.8",
-  "./foresta-questions.js?v=7.8",
-  "./app.js?v=7.8",
-  "./manifest.webmanifest?v=7.8",
+  "./spell-dictionary.js?v=7.9",
+  "./foresta-questions.js?v=7.9",
+  "./app.js?v=7.9",
+  "./manifest.webmanifest?v=7.9",
   "./icons/icon-64.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
